0f812635b0eedae46e8ff4ce0b655131e9f92410
SB <you@example.com>
2019-09-20 12:14:29 +0900

    add method

--- Animal.java
+++ Animal.java
@@ -2,13 +2,21 @@ package gitTest;
 
 public class Animal {
 
-	public void play() {
+	public void play1() {
 		System.out.println("港港");
+	}
+	public void play2() {
+		System.out.println("具克");
+	}
+	public void play3() {
+		System.out.println("卤卤");
 	}
 
 	public static void main(String[] args) {
 		Animal a =new Animal();
-		a.play();
+		a.play1();
+		a.play2();
+		a.play3();
 	}
 
 }
